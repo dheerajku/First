a9c6f2bf11ff3934a312991d21b52c7b8f6b55b3
dheerajku <dheerajkushwah2015@gmail.com>
2018-08-26 01:36:31 +0530

    Create Skiil model in model-generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_25_182343) do
+ActiveRecord::Schema.define(version: 2018_08_25_195307) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent_utilize"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
